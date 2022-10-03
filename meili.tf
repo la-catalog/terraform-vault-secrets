d16248bc4a590b5f888159221604b25a2abbb9ce
thiagola92 <thiagola92@users.noreply.github.com>
2022-10-03 05:06:59 -0300

    Workflow fix

--- meili.tf
+++ meili.tf
@@ -7,7 +7,7 @@ resource "vault_kv_secret" "meili" {
   path = "${vault_mount.url.path}/meili"
   data_json = jsonencode(
     {
-      MEILI_URL = "http://${var.machine_ip}:7700"
+      MEILI_URL  = "http://${var.machine_ip}:7700"
       MEILI_PASS = var.meili_pass
     }
   )
