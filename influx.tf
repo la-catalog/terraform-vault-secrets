e8e1f49388e4316d059875913987eac257c5a466
thiagola92 <thiagola92@gmail.com>
2022-11-29 08:26:49 -0300

    Add influx user and pass

--- influx.tf
+++ influx.tf
@@ -3,10 +3,22 @@ resource "vault_mount" "influx" {
   type = "kv"
 }
 
+variable "influx_main_user" {
+  type      = string
+  sensitive = true
+}
+
+variable "influx_main_pass" {
+  type      = string
+  sensitive = true
+}
+
 resource "vault_kv_secret" "influx_main" {
   path = "${vault_mount.influx.path}/main"
   data_json = jsonencode(
     {
+      INFLUX_USER = var.influx_main_user
+      INFLUX_PASS = var.influx_main_pass
       INFLUX_URL = "http://${var.machine_ip}:8086"
     }
   )
